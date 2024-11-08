412ee48b2271491218e24ae30cb80ec9689f1824
Abdel-manan Abdel-rahman <abdelmanan.abdelrahman03@gmail.com>
2024-11-08 16:58:11 +0300

    v1 backpass- tho not neccessary

--- flash_attn.cu
+++ flash_attn.cu
@@ -17,7 +17,7 @@
 
 #define TILE_DIM 32 // Tile dimension for efficient memory usage
 
-// this is the kernel for flash attention v1
+// THIS IS THE FORWARD PASS FOR FLASH ATTENTION V1 KERNEL
 __global__ void forward1_kernel(const float *Q, const float *K, const float *V,
                                 const int N, const int d, const int Tc,
                                 const int Tr, const int Bc, const int Br,
@@ -102,8 +102,160 @@ __global__ void forward1_kernel(const float *Q, const float *K, const float *V,
   }
 }
 
+// THIS IS THE BACKWARD PASS FOR THE FLASHATTENTION V1 KERNEL.
+// NOTE: FlashAttention’s main performance gains are seen during the forward
+// pass, particularly in inference. While this backward pass is required for
+// training, it may not benefit as much from FlashAttention's optimizations.
+__global__ void backward1_kernel(const float *Q, const float *K, const float *V,
+                                 const float *O, const float *dO,
+                                 const float *l, const float *m, const int N,
+                                 const int d, const int Tc, const int Tr,
+                                 const int Bc, const int Br,
+                                 const float softmax_scale, float *dQ,
+                                 float *dK, float *dV) {
+  int tx = threadIdx.x;
+  int bx = blockIdx.x;
+  int by = blockIdx.y; // batch and head index
+
+  // Offset into Q,K,V,O,l,m - different for each batch and head
+  int qkv_offset = (bx * gridDim.y * N * d) + (by * N * d); // gridDim.y = nh
+  int lm_offset = (bx * gridDim.y * N) + (by * N); // offset for l and m
+
+  // Define SRAM for Q,K,V,S
+  extern __shared__ float sram[];
+  int col_tile_size = Bc * d; // size of Kj, Vj
+  int row_tile_size = Br * d; // size of Qi
+  float *Kj = sram;
+  float *Vj = &sram[col_tile_size];
+
+  float *dKj = &sram[col_tile_size * 2];
+  float *dVj = &sram[col_tile_size * 3];
+
+  float *Qi = &sram[col_tile_size * 4];
+  float *Oi = &sram[col_tile_size * 4 + row_tile_size];
+  float *dOi = &sram[col_tile_size * 4 + row_tile_size * 2];
+
+  // We also use S for P. Likewise, we use dS for dP.
+  // We can reuse the same memory because we don't need S and P at the same
+  // time. We also don't need dS and dP at the same time.
+  float *S = &sram[col_tile_size * 4 + row_tile_size * 3];
+  float *dS = &sram[col_tile_size * 4 + row_tile_size * 3 + Bc * Br];
+
+  for (int j = 0; j < Tc; j++) {
+
+    // Load Kj, Vj to SRAM
+    for (int x = 0; x < d; x++) {
+      Kj[(tx * d) + x] = K[qkv_offset + (col_tile_size * j) + (tx * d) + x];
+      Vj[(tx * d) + x] = V[qkv_offset + (col_tile_size * j) + (tx * d) + x];
+    }
+
+    // Initialize dKj, dVj to 0
+    for (int x = 0; x < d; x++) {
+      dKj[(tx * d) + x] = 0;
+      dVj[(tx * d) + x] = 0;
+    }
+
+    for (int i = j; i < Tr; i++) {
+      __syncthreads();
+      // Load Qi, Oi, dOi, dQi, li, mi to SRAM
+      // Also load l, m to registers
+      for (int x = 0; x < d; x++) {
+        Qi[(tx * d) + x] = Q[qkv_offset + (row_tile_size * i) + (tx * d) + x];
+        Oi[(tx * d) + x] = O[qkv_offset + (row_tile_size * i) + (tx * d) + x];
+        dOi[(tx * d) + x] = dO[qkv_offset + (row_tile_size * i) + (tx * d) + x];
+      }
+      float m_curr = m[lm_offset + (Br * i) + tx];
+      float l_curr = l[lm_offset + (Br * i) + tx];
+
+      // Sij = softmax_scale * QiKj^T
+      // Sij[tx][y] = softmax_scale * Sum_{y = 0}^{Bc-1} Qi[tx][x] * Kj[y][x]
+      for (int y = 0; y < Bc; y++) {
+        float sum = 0;
+        for (int x = 0; x < d; x++) {
+          sum += Qi[(tx * d) + x] * Kj[(y * d) + x];
+        }
+        sum *= softmax_scale;
+        if (i * Br + tx < j * Bc + y)
+          sum = -INFINITY;
+        S[(Bc * tx) + y] = sum;
+      }
+
+      // Pij = diag(li)^-1 * exp(Sij - mi)
+      // Pij[tx][y] = (1 / li[tx]) * exp(Sij[tx][y] - mi[tx])
+      for (int y = 0; y < Bc; y++) {
+        if (i * Br + tx < j * Bc + y)
+          S[(Bc * tx) + y] = 0;
+        else
+          S[(Bc * tx) + y] = (1 / l_curr) * __expf(S[(Bc * tx) + y] - m_curr);
+      }
+      __syncthreads();
+      // dVj <- dVj + Pij^T * dOi
+      // dVj[tx][x] = dVj[tx][x] + Sum_{y = 0}^{Br-1} Pij[y][tx] * dOi[tx][x]
+      for (int x = 0; x < d; x++) {
+        float sum = 0;
+        for (int y = 0; y < Br; y++) {
+          sum += S[(Bc * y) + tx] * dOi[(tx * d) + x];
+        }
+        atomicAdd(&dVj[(tx * d) + x], sum);
+      }
+
+      // dPij <- dOi * Vj^T
+      // dPij[tx][y] = Sum_{x = 0}^{d-1} dOi[tx][x] * Vj[y][x]
+      for (int y = 0; y < Bc; y++) {
+        float sum = 0;
+        for (int x = 0; x < d; x++) {
+          sum += dOi[(tx * d) + x] * Vj[(y * d) + x];
+        }
+        dS[(Bc * tx) + y] = sum;
+      }
+
+      // Di <- rowsum(dOi * Oi)  (pointwise multiply)
+      // Di[tx] = Sum_{x = 0}^{d-1} dOi[tx][x] * Oi[tx][x]
+      float Di = 0;
+      for (int x = 0; x < d; x++) {
+        Di += dOi[(tx * d) + x] * Oi[(tx * d) + x];
+      }
+
+      // dSij <- Pij * (dPij - Di)
+      // dSij[tx][y] = Pij[tx][y] * (dPij[tx][y] - Di[tx])
+      for (int y = 0; y < Bc; ++y) {
+        dS[(Bc * tx) + y] = S[(Bc * tx) + y] * (dS[(Bc * tx) + y] - Di);
+      }
+
+      // dQi <- dQi + softmax_scale * dSijKj
+      // dQ[tx][x] = dQ[tx][x] + softmax_scale * Sum_{y = 0}^{Bc-1} dSij[tx][y]
+      // * Kj[y][x]
+      for (int x = 0; x < d; x++) {
+        float sum = 0;
+        for (int y = 0; y < Bc; y++) {
+          sum += dS[(Bc * tx) + y] * Kj[(y * d) + x];
+        }
+        sum *= softmax_scale;
+        atomicAdd(&dQ[qkv_offset + (row_tile_size * i) + (tx * d) + x], sum);
+      }
+      __syncthreads();
+      // dKj <- dKj + softmax_scale * dSij^TQi
+      // dKj[tx][x] = dKj[tx][x] + softmax_scale * Sum_{y = 0}^{Br-1}
+      // dSij[y][tx] * Qi[y][x]
+      for (int x = 0; x < d; x++) {
+        float sum = 0;
+        for (int y = 0; y < Br; y++) {
+          sum += dS[(Bc * y) + tx] * Qi[(y * d) + x];
+        }
+        sum *= softmax_scale;
+        atomicAdd(&dKj[(tx * d) + x], sum);
+      }
+    }
+
+    // Upload Kj, Vj to HRAM
+    for (int x = 0; x < d; x++) {
+      dK[qkv_offset + (row_tile_size * j) + (tx * d) + x] = dKj[(tx * d) + x];
+      dV[qkv_offset + (row_tile_size * j) + (tx * d) + x] = dVj[(tx * d) + x];
+    }
+  }
+}
+
 torch::Tensor forward(torch::Tensor Q, torch::Tensor K, torch::Tensor V) {
-  // TODO: determine Bc, Br dynamically
 
   const int B = Q.size(0);                   // batch size
   const int nh = Q.size(1);                  // number of heads
@@ -138,19 +290,10 @@ torch::Tensor forward(torch::Tensor Q, torch::Tensor K, torch::Tensor V) {
   return O;
 }
 
-// this is the kernel for flash attention v1
-__global__ void backward1_kernel(const float *Q, const float *K, const float *V,
-                                 const float *O, const float *dO,
-                                 const float *l, const float *m, const int N,
-                                 const int d, const int Tc, const int Tr,
-                                 const int Bc, const int Br,
-                                 const float softmax_scale, float *dQ,
-                                 float *dK, float *dV) {}
 std::vector<torch::Tensor> backward(torch::Tensor Q, torch::Tensor K,
                                     torch::Tensor V, torch::Tensor O,
                                     torch::Tensor dO, torch::Tensor l,
                                     torch::Tensor m, ) {
-  // TODO: determine Bc, Br dynamically
 
   const int B = Q.size(0);                   // batch size
   const int nh = Q.size(1);                  // number of heads
