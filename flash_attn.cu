223da9e91b3630d21ddf67a8aab484586e60ba16
Abdel-manan Abdel-rahman <abdelmanan.abdelrahman03@gmail.com>
2024-11-08 16:46:33 +0300

    flash attention basic implementation

---
+++ flash_attn.cu
@@ -0,0 +1,189 @@
+// resources:
+// - https://github.com/tspeterkim/flash-attention-minimal
+// - https://github.com/leloykun/flash-hyperbolic-attention-minimal
+// - https://github.com/karpathy/llm.c
+
+#include <cmath>
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <sys/types.h>
+#include <torch/types.h>
+
+// #define ceil_div(x, y) (ceil((float)(x) / y))
+#define ceil_div(x, y) ((x + y - 1) / y)
+// a basic reimplementation of flash attention v1 paper.
+// Note: I'm new to CUDA, so might not be 100% accurate
+//
+
+#define TILE_DIM 32 // Tile dimension for efficient memory usage
+
+// this is the kernel for flash attention v1
+__global__ void forward1_kernel(const float *Q, const float *K, const float *V,
+                                const int N, const int d, const int Tc,
+                                const int Tr, const int Bc, const int Br,
+                                const float softmax_scale, float *l, float *m,
+                                float *O) {
+  int tx = threadIdx.x;
+  int bx = blockIdx.x;
+  int by = blockIdx.y; // batch and head index
+
+  // Offset into Q,K,V,O,l,m - different for each batch and head
+  int qkv_offset = (bx * gridDim.y * N * d) + (by * N * d); // gridDim.y = nh
+  int lm_offset = (bx * gridDim.y * N) + (by * N); // offset for l and m
+
+  // Define SRAM for Q,K,V,S
+  extern __shared__ float sram[];
+  int tile_size = Bc * d; // size of Qi, Kj, Vj
+  float *Qi = sram;
+  float *Kj = &sram[tile_size];
+  float *Vj = &sram[tile_size * 2];
+  float *S = &sram[tile_size * 3];
+
+  for (int j = 0; j < Tc; j++) {
+
+    // Load Kj, Vj to SRAM
+    for (int x = 0; x < d; x++) {
+      Kj[(tx * d) + x] = K[qkv_offset + (tile_size * j) + (tx * d) + x];
+      Vj[(tx * d) + x] = V[qkv_offset + (tile_size * j) + (tx * d) + x];
+    }
+    __syncthreads(); // such that the inner loop can use the correct Kj, Vj
+
+    for (int i = 0; i < Tr; i++) {
+
+      // Load Qi to SRAM, l and m to registers
+      for (int x = 0; x < d; x++) {
+        Qi[(tx * d) + x] = Q[qkv_offset + (tile_size * i) + (tx * d) + x];
+      }
+      float row_m_prev = m[lm_offset + (Br * i) + tx];
+      float row_l_prev = l[lm_offset + (Br * i) + tx];
+
+      // S = QK^T, row_m = rowmax(S)
+      float row_m = -INFINITY;
+      for (int y = 0; y < Bc; y++) {
+        float sum = 0;
+        for (int x = 0; x < d; x++) {
+          sum += Qi[(tx * d) + x] * Kj[(y * d) + x];
+        }
+        sum *= softmax_scale;
+        S[(Bc * tx) + y] = sum;
+
+        if (sum > row_m)
+          row_m = sum;
+      }
+
+      // P = exp(S - row_m), row_l = rowsum(P)
+      float row_l = 0;
+      for (int y = 0; y < Bc; y++) {
+        S[(Bc * tx) + y] = __expf(S[(Bc * tx) + y] - row_m);
+        row_l += S[(Bc * tx) + y];
+      }
+
+      // Compute new m and l
+      float row_m_new = max(row_m_prev, row_m);
+      float row_l_new = (__expf(row_m_prev - row_m_new) * row_l_prev) +
+                        (__expf(row_m - row_m_new) * row_l);
+
+      // Write O, l, m to HBM
+      for (int x = 0; x < d; x++) {
+        float pv = 0; // Pij * Vj
+        for (int y = 0; y < Bc; y++) {
+          pv += S[(Bc * tx) + y] * Vj[(y * d) + x];
+        }
+        O[qkv_offset + (tile_size * i) + (tx * d) + x] =
+            (1 / row_l_new) *
+            ((row_l_prev * __expf(row_m_prev - row_m_new) *
+              O[qkv_offset + (tile_size * i) + (tx * d) + x]) +
+             (__expf(row_m - row_m_new) * pv));
+      }
+      m[lm_offset + (Br * i) + tx] = row_m_new;
+      l[lm_offset + (Br * i) + tx] = row_l_new;
+    }
+    __syncthreads(); // otherwise, thread can use the wrong Kj, Vj in inner loop
+  }
+}
+
+torch::Tensor forward(torch::Tensor Q, torch::Tensor K, torch::Tensor V) {
+  // TODO: determine Bc, Br dynamically
+
+  const int B = Q.size(0);                   // batch size
+  const int nh = Q.size(1);                  // number of heads
+  const int T = Q.size(2);                   // sequence length
+  const int C = Q.size(3);                   // number of channels
+  const int Tc = ceil_div(T, TILE_DIM);      // number of tiles along columns
+  const int Tr = ceil_div(T, TILE_DIM);      // number of tiles along rows
+  const float softmax_scale = 1.0 / sqrt(C); // scaling factor in attention
+
+  auto O = torch::zeros_like(Q);     // output tensor
+  auto l = torch::zeros({B, nh, N}); // storing softmax computed values
+  auto m = torch::full({B, nh, N}, -INFINITY); // stores max values
+                                               //
+  // SRAM size needed per block, checkout
+  // `./quadratic_attention.ipynb` for explanation
+  const int sram_size =
+      (3 * Bc * d * sizeof(float)) + (Bc * Br * sizeof(float));
+
+  int max_sram_size;
+  cudaDeviceGetAttribute(&max_sram_size, cudaDevAttrMaxSharedMemoryPerBlock, 0);
+  printf("Maximum shared memory: %d\nRequested shared memory: %d \n",
+         max_sram_size, sram_size);
+
+  dim3 grid_dim(B, nh);     // batch size * number of heads
+  dim3 block_dim(TILE_DIM); // Batch threads per tile
+
+  forward1_kernel(Q.data_ptr<float>(), K.data_ptr<float>(), V.data_ptr<float>(),
+                  T, C, Tc, Tr, TILE_DIM, TILE_DIM, softmax_scale,
+                  l.data_ptr<float>(), m.data_ptr<float>(),
+                  O.data_ptr<float>());
+  )
+  return O;
+}
+
+// this is the kernel for flash attention v1
+__global__ void backward1_kernel(const float *Q, const float *K, const float *V,
+                                 const float *O, const float *dO,
+                                 const float *l, const float *m, const int N,
+                                 const int d, const int Tc, const int Tr,
+                                 const int Bc, const int Br,
+                                 const float softmax_scale, float *dQ,
+                                 float *dK, float *dV) {}
+std::vector<torch::Tensor> backward(torch::Tensor Q, torch::Tensor K,
+                                    torch::Tensor V, torch::Tensor O,
+                                    torch::Tensor dO, torch::Tensor l,
+                                    torch::Tensor m, ) {
+  // TODO: determine Bc, Br dynamically
+
+  const int B = Q.size(0);                   // batch size
+  const int nh = Q.size(1);                  // number of heads
+  const int T = Q.size(2);                   // sequence length
+  const int C = Q.size(3);                   // number of channels
+  const int Tc = ceil_div(T, TILE_DIM);      // number of tiles along columns
+  const int Tr = ceil_div(T, TILE_DIM);      // number of tiles along rows
+  const float softmax_scale = 1.0 / sqrt(C); // scaling factor in attention
+
+  auto dQ = torch::zeros_like(Q); // gradients of query tensor
+  auto dK = torch::zeros_like(K); // gradients of key tensor
+  auto dV = torch::zeros_like(V); // gradients of value tensor
+
+  // SRAM size needed per block, checkout
+  // `./quadratic_attention.ipynb` for explanation
+  const int sram_size =
+      (4 * col_tile_size * sizeof(float))   // SRAM size for Kj, Vj, dKj, dVj
+      + (3 * row_tile_size * sizeof(float)) // SRAM size for Qi, Oi, dOi
+      + (2 * Br * Bc * sizeof(float));      // SRAM size for S, dS
+
+  int max_sram_size;
+  cudaDeviceGetAttribute(&max_sram_size, cudaDevAttrMaxSharedMemoryPerBlock, 0);
+  printf("Maximum shared memory: %d\nRequested shared memory: %d \n",
+         max_sram_size, sram_size);
+
+  dim3 grid_dim(B, nh);     // batch size * number of heads
+  dim3 block_dim(TILE_DIM); // Batch threads per tile
+
+  backward1_kernel<<<grid_dim, block_dim, sram_size>>>(
+      Q.data_ptr<float>(), K.data_ptr<float>(), V.data_ptr<float>(),
+      O.data_ptr<float>(), dO.data_ptr<float>(), l.data_ptr<float>(),
+      m.data_ptr<float>(), T, C, Tc, Tr, TILE_DIM, TILE_DIM, softmax_scale,
+      dQ.data_ptr<float>(), dK.data_ptr<float>(), dV.data_ptr<float>(), );
+  )
+      return {dQ, dK, dV};
+}
